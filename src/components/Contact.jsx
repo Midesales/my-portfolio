e01c0d3c3d90b5f2f9f9b4225f7b2eede8719f3c
Ayomide <vayomide2@gmail.com>
2024-05-08 12:40:11 +0100

    added the submit form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,52 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import leaf1 from "../images/Leaf1.png";
 import leaf2 from "../images/leaf2.png";
 import { motion } from "framer-motion";
+import emailjs from "@emailjs/browser";
 
 function Contact() {
+  const form = useRef();
+  const [errorMessage, setErrorMessage] = useState(false);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    // Validate form fields
+    const fullName = form.current["user_name"].value;
+    const email = form.current["user_email"].value;
+    const message = form.current["message"].value;
+
+    if (!fullName || !email || !message) {
+      setErrorMessage(true);
+      setTimeout(() => {
+        setErrorMessage(false);
+      }, 3000);
+      // alert('Please fill in all the details.'); // Display error message
+      return;
+    }
+
+    // Send email
+    emailjs
+      .sendForm("service_pfwtb0r", "template_s3lmt6o", form.current, {
+        publicKey: "NMYq4lorpR4cTriJO",
+      })
+      .then(
+        (response) => {
+          e.target.reset();
+          alert("Email successfully sent!");
+          console.log("SUCCESS!", response.status, response.text);
+        },
+        (error) => {
+          console.log("FAILED...", error);
+        }
+      );
+  };
+
   return (
-    <section id="contact" className="text-white averia-libre-light flex flex-col items-center justify-center my-10 gap-12">
+    <section
+      id="contact"
+      className="text-white averia-libre-light flex flex-col items-center justify-center my-10 gap-12"
+    >
       <div className="flex items-center justify-center w-full py-14">
         <motion.div
           initial={{ opacity: 0, width: 0 }}
@@ -28,36 +69,45 @@ function Contact() {
         </motion.div>
       </div>
       <div className="w-full flex flex-col justify-start items-center gap-4">
-        <div className="w-full p-2 lg:w-[600px] flex flex-col justify-start items-start gap-4">
-          <div className="w-full grid grid-cols-1 lg:grid-cols-2 gap-4">
-            <input
-              type="text"
-              placeholder="First name"
-              className="p-2 bg-transparent outline-none border border-slate-100 rounded-md"
-            />
-            <input
-              type="text"
-              placeholder="Last name"
-              className="p-2 bg-transparent outline-none border border-slate-100 rounded-md"
-            />
-          </div>
+        {errorMessage && (
+          <p className="font-bold text-red-500">Please fill in the details</p>
+        )}
+        <form
+          ref={form}
+          onSubmit={sendEmail}
+          className="w-full p-2 lg:w-[600px] flex flex-col justify-start items-start gap-4"
+        >
+          <input
+            type="text"
+            name="from_name"
+            placeholder="Enter Full Name"
+            className="w-full p-2 bg-transparent outline-none border border-slate-100 rounded-md"
+          />
+
           <input
             type="email"
+            name="from_email"
             placeholder="Email"
             className="w-full p-2 bg-transparent outline-none border border-slate-100 rounded-md"
           />
           <textarea
-            name="Message"
+            name="message"
             id=""
             cols="0"
             rows="10"
-            placeholder="Message here"
+            placeholder="Type your message here"
             className="w-full p-2 bg-transparent outline-none border border-slate-100 rounded-md"
-          ></textarea>
+          />
           <div className="w-full flex justify-center items-center lg:justify-end">
-            <button type="submit" className="px-6 py-4 rounded-md bg-orange-400 w-full lg:w-auto hover:bg-slate-200 hover:text-orange-400">Send</button>
+            <button
+              type="submit"
+              value="Send"
+              className="px-6 py-4 rounded-md bg-orange-400 w-full lg:w-auto hover:bg-slate-200 hover:text-orange-400"
+            >
+              Send
+            </button>
           </div>
-        </div>
+        </form>
       </div>
     </section>
   );
